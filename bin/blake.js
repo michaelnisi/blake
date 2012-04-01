770cbd28a954a2cd7e27de3a3a4a76d31eac9a16
Michael Nisi <michael.nisi@gmail.com>
2012-04-01 19:57:27 +0200

    Add colors

--- bin/blake.js
+++ bin/blake.js
@@ -2,6 +2,7 @@
 
 // This is the CLI-wrapper of Blake.
 
+
 // Receive and validate command-line arguments. If usage is incorrect, exit
 // with a usage prompt; otherwise call bake with the provided paths to the 
 // input and output directories. Exit the process when done. 
@@ -13,16 +14,24 @@
     return console.error('Usage: blake path/to/input path/to/output [path/to/input/file …]');
   }
 
-  var ok = '\u001b[32mOK\u001b[39m';
-
+  var color = require('../lib/color.js');
+  var red = color.red;
+  var green = color.green;
+  
+  var ok = green('OK');
+  var ko = red('Not OK');
+  
   console.time(ok);
+  console.time(ko);
 
   require('../lib/blake.js').bake(arg, function(err) {
     if (err) {
-      throw err;
+      console.error(red('Err: %s'), err.message); 
+      console.timeEnd(ko); 
+    } else {
+      console.timeEnd(ok);
     }
 
-    console.timeEnd(ok);
     process.exit();
   });
 })();
