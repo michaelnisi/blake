279cca9322706befe550130e7199cd2717b5918d
Michael Nisi <michael.nisi@gmail.com>
2012-07-05 23:34:20 +0200

    Rewrite all

--- test/foundation.js
+++ test/foundation.js
@@ -1,12 +1,20 @@
 var test = require('tap').test
 ,   blake = require('../lib/blake.js')
-,   fstream = require('fstream')
+,   statSync = require('fs').statSync
+,   resolve = require('path').resolve
 
 test('foundation', function (t) {
-  // t.ok(writer.config, 'should be there')
-  // t.ok(writer.templates, 'should be there')
+  var source = 'source'
+  ,   target = 'target'
 
-  blake('source', 'target', function (err) {
+  blake(source, target, function (err) {
+    t.ok(statSync(resolve(target, 'index.html')).isFile())
+   
+    var dirs = ['css', 'img', 'js', '2011', '2012']
+    dirs.forEach(function (path) {
+      t.ok(statSync(resolve(target, path)).isDirectory())
+    }) 
+   
     t.end() 
   })
 })
