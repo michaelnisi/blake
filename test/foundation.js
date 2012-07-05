3bcdc1dd89e2afacf04fd6726cab0f2ca53b6046
Michael Nisi <michael.nisi@gmail.com>
2012-07-05 11:23:04 +0200

    Slim fast

--- test/foundation.js
+++ test/foundation.js
@@ -3,14 +3,10 @@ var test = require('tap').test
 ,   fstream = require('fstream')
 
 test('foundation', function (t) {
-  var r = fstream.Reader('source')
-  ,   w = fstream.Writer({ path:'target', type:'Directory'})
-  ,   b = blake('source')
+  // t.ok(writer.config, 'should be there')
+  // t.ok(writer.templates, 'should be there')
 
-  t.ok(b.config, 'should be there')
-  t.ok(b.templates, 'should be there')
-
-  r.pipe(b).pipe(w)
+  blake('source', 'target')
 
   t.end()
 })
