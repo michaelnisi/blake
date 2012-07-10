95d18f4d4faccb00fff5ea0b3c1d5835f0c34280
Michael Nisi <michael.nisi@gmail.com>
2012-07-10 10:13:44 +0200

    Write tests

--- bin/cli.js
+++ bin/cli.js
@@ -1,19 +1,19 @@
 #!/usr/bin/env node
 
 var blake = require('../lib/blake.js')
-,   resolve = require('path').resolve
+  , resolve = require('path').resolve
 
 ;(function () {
   var arg = process.argv.splice(2)
-  ,   isValid = !(arg && arg.length >= 2)
+    , isValid = !(arg && arg.length >= 2)
 
   if (isValid) {
     return console.error('Usage: blake source_directory target_directory [source_file ...]');
   }
   
   var source = arg[0]
-  ,   target = arg[1]
-  ,   config = require(resolve(source, 'views', 'config.js'))
+    , target = arg[1]
+    , config = require(resolve(source, 'views', 'config.js'))
 
   blake(source, target, config, function (err) {
     if (err) return console.error(err)
