21e0b4b0d88a0d82ec11a3bd3c0406847a53a817
Michael Nisi <michael.nisi@gmail.com>
2012-10-06 14:55:43 +0200

    Stream

--- example/generate.js
+++ example/generate.js
@@ -1,12 +1,14 @@
 var blake = require('blake')
+  , source = '/Users/michael/workspace/troubled'
+  , target = '/tmp/troubled-site'
+  , resolve = require('path').resolve
+  , Reader = require('fstream').Reader
+  , props = { path:resolve(source, 'data') }
+  , reader = new Reader(props)
+  , cop = require('cop')
 
-blake('blake-site', '/tmp/blake-site')
-  .on('data', function (item) {
-    console.log(item)
-  })
-  .on('error', function (err) {
-    console.error(err)
-  })
-  .on('end', function () {
-    console.log('OK')
-  })
+reader
+  .pipe(cop('path'))
+  .pipe(blake(source, target))
+  .pipe(cop(function (filename) { return filename + '\n' }))
+  .pipe(process.stdout)
--- index.js
+++ index.js
@@ -2,74 +2,23 @@
 
 module.exports = blake
 
-var resolve = require('path').resolve
-  , fstream = require('fstream')
-  , fs = require('fs')
-  , es = require('event-stream')
-  , generate = require('./lib/generate.js')
+var generate = require('./lib/generate.js')
   , copy = require('./lib/copy.js')
-  , popfun = require('popfun')
+  , resolve = require('path').resolve
 
-function blake () {
-  var args = Array.prototype.slice.call(arguments)
-    , source = args.shift()
-    , target = args.shift()
-    , callback = popfun(args)
-    , config = require(resolve(source, 'config.js'))
+function blake (source, target) {
+  var config = require(resolve(source, 'config.js'))
     , paths = require('./lib/paths.js')(source, target, config)
     , templates = require('./lib/templates.js')(paths.templates)
     , views = config.views
-    , specific = !!args.length && !!args[0].length
-    , files = specific ? (Array.isArray(args[0]) ? args[0] : args) : null   
+
   var props = {
     templates: templates
   , views: views
   , paths: paths
   }
 
-  var writer = generate(props)
+  // copy
 
-  if (specific) {
-    bake()
-  } else {
-    copy(paths.resources, target)
-      .on('error', function (err) {
-        console.error(err)
-      })
-      .on('end', function () {
-        bake()
-      })
-  }
-
-  function bake () {
-    var reader = getReader()  
-    
-    reader.pipe(writer)
-
-    writer.on('resume', function () {
-      reader.resume()
-    })
-
-    writer.on('end', function () {
-      if (callback) callback()
-    })
-  }
-
-  function getReader() {
-    var stream
-
-    if (specific) {
-      var entries = []
-      files.forEach(function (file) {
-        entries.push({ path: file })
-      })
-      stream = es.readArray(entries)
-    } else {
-      stream = fstream.Reader({ path: resolve(source, 'data') })
-    }
-
-    return stream
-  }
-
-  return writer
+  return generate(props)
 }
