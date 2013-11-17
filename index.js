eff2386902c853c7450ff5932b308db44d247522
Michael Nisi <michael.nisi@gmail.com>
2013-11-17 19:07:29 +0100

    Review index

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ var Transform = require('stream').Transform
   , writeFile = require('fs').writeFile
   , StringDecoder = require('string_decoder').StringDecoder
   , read = require('./lib/read.js').readItems
-  , getConf = require('./lib/read.js').conf
+  , conf = require('./lib/read.js').conf
 
 module.exports = function (s, target) {
-  var conf = s && target ? getConf(s, target) : s
+  var c = s && target ? conf(s, target) : s
     , decoder = new StringDecoder('utf8')
     , stream = new Transform({ objectMode:true })
 
   stream._transform = function (chunk, encoding, cb) {
     var filename = decoder.write(chunk)
-    read(conf)(filename, function (er, item) {
+    read(c)(filename, function (er, item) {
       bake(item, function (er) {
         stream.push(item.path)
         cb(er)
@@ -29,7 +29,7 @@ module.exports = function (s, target) {
       cb(new Error('Undefined bake function for ' + item.name))
       return
     }
-    item.read = read(conf)
+    item.read = read(c)
     item.bake(item, function (er, result) {
       if (er) {
         stream.emit('error', er)
